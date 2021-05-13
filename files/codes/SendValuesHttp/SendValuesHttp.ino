7148b99ff46ecfd8ae374b014a7c34c468120c08
Marco Teran <marco.tulio.teran@gmail.com>
2021-05-13 12:27:47 -0500

    added ubidota examp

--- files/codes/SendValuesHttp/SendValuesHttp.ino
+++ files/codes/SendValuesHttp/SendValuesHttp.ino
@@ -28,16 +28,6 @@ Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
 /****************************************
  * Auxiliar Functions
  ****************************************/
-
-void readSensor(){
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial.print("Temperatura: ");
-  Serial.println(t);
-  Serial.print("Humedad: ");
-  Serial.println(h);
-}
-
 void checkPin(){
   ext_intFlag = !ext_intFlag;
   pinChanged = true;
@@ -62,14 +52,42 @@ void setup() {
 
 void loop() {
 
-  readSensor();
+  float value = random(0, 9) * 10;
   
-  float value1 = random(0, 9) * 10;
-  float value2 = random(0, 9) * 100;
-  float value3 = random(0, 9) * 1000;
-  ubidots.add("Variable_Name_One", value1);  // Change for your variable name
-  ubidots.add("Variable_Name_Two", value2);
-  ubidots.add("Variable_Name_Three", value3);
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial.print("Temperatura: ");
+  Serial.println(t);
+  Serial.print("Humedad: ");
+  Serial.println(h);
+    
+  ubidots.add("temperatura_marco", t);  // Change for your variable name
+  ubidots.add("humedad_marco", h);
+
+  /* Hardcoded Coordinates */
+  float latitude = 4.660984864922051;
+  float longitude = -74.05983339924464;
+
+  /* Reserves memory to store context key values, add as much as you need */
+  char* str_lat = (char*)malloc(sizeof(char) * 10);
+  char* str_lng = (char*)malloc(sizeof(char) * 10);
+
+  /* Saves the coordinates as char */
+  sprintf(str_lat, "%f", latitude);
+  sprintf(str_lng, "%f", longitude);
+
+  /* Reserves memory to store context array */
+  char* context = (char*)malloc(sizeof(char) * 30);
+
+  /* Adds context key-value pairs */
+  ubidots.addContext("lat", str_lat);
+  ubidots.addContext("lng", str_lng);
+
+  /* Builds the context with the coordinates to send to Ubidots */
+  ubidots.getContext(context);
+
+  /* Sends the position */
+  ubidots.add("position", value, context);  // Change for your variable name
 
   bool bufferSent = false;
   bufferSent = ubidots.send();  // Will send data to a device label that matches the device Id
